d9316bb931915702ec40e3c3b52bf60b33f51131
scattamooch <seanstevens729@gmail.com>
2023-10-05 14:08:04 -0400

    Initialized. Create/join room views and urls. HomePage view and urls. Room and CreateRoom Serializers. Front end skeleton.

---
+++ Harmonize_App/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class CreateRoomPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is where we will create a room</p>
+      </div>
+    );
+  }
+}
---
+++ Harmonize_App/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {render} from "react-dom";
+
+export default class RoomJoinPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <p>This is where we join a room</p>
+        )
+    }
+}
